894f77c33ecf7de394361be4b15a1e51b74505f6
Dmitry Koprov <dmitry.koprov@evrone.ru>
2013-07-01 15:02:22 +0400

    typo fix; refactor

--- zsh/lib/propmt.zsh
+++ zsh/lib/propmt.zsh
@@ -3,18 +3,14 @@ setopt prompt_subst
 
 git_branch () {
   ref=$(/usr/bin/git symbolic-ref HEAD 2> /dev/null | awk -F/ {'print $NF'})
-  if [[ $ref == "" ]]; then
-    echo ""
-  else
+  if [[ $ref != "" ]]; then
     echo " %{$fg[yellow]%}$ref%{$reset_color%}"
   fi
 }
 
 git_dirty () {
   st=$(/usr/bin/git status 2> /dev/null)
-  if [[ $st == "" ]]; then
-    echo ""
-  else
+  if [[ $st != "" ]]; then
     local -a arr
     arr=(${(f)st})
     if [[ $st =~ "nothing to commit" ]]; then
@@ -24,15 +20,13 @@ git_dirty () {
   fi
 }
 
-git_shashes_count () {
+git_stashes_count () {
   st_num=$(/usr/bin/git stash list 2> /dev/null | wc -l | tr -d ' ')
-  if [[ $st_num == "0" ]]; then
-    echo ""
-  else
+  if [[ $st_num != "0" ]]; then
     echo " %{$fg[red]%} ($st_num)%{$reset_color%}"
   fi
 }
 
 # wo ruby info
-export PROMPT='%{$reset_color%}%{$fg[blue]%}%c%{$reset_color%} $(git_branch)$(git_dirty)$(git_shashes_count)
+export PROMPT='%{$reset_color%}%{$fg[blue]%}%c%{$reset_color%} $(git_branch)$(git_dirty)$(git_stashes_count)
 %# '
